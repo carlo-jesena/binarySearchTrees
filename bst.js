4ee1a5b0ccf1232e9ce0381626ac2942e8d63210
Your Namecarlo <carlo.jesena@gmail.com>
2017-01-09 21:53:48 -0800

    initialized binary search tree

---
+++ bst.js
@@ -0,0 +1,7 @@
+var BinarySearchTree = function(key, value, parent) {
+  this.key = key || null;
+  this.value = value || null;
+  this.parent = parent || null;
+  this.left = null;
+  this.right = null;
+}
