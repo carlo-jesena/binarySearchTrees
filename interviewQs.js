af70ae674eaa2ae7d237817c3da2c5c6195b996c
Your Namecarlo <carlo.jesena@gmail.com>
2017-01-09 21:47:19 -0800

    first commit

---
+++ interviewQs.js
@@ -0,0 +1,11 @@
+// 1) Write an algorithm to check whether an
+//    arbitrary tree is a binary search tree.
+
+
+
+// 2) Write an algorithm to find the height
+//    of a binary search tree.
+
+
+// 3) Write an algorithm to find the third
+//    largest value in a binary search tree.
